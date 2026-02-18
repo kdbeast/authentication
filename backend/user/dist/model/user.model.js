25e3d16ce861be7f877f71deb91a4d32a0de75ce
Karan Jamwal <karanjamwal2004@gmail.com>
2026-02-18 19:02:39 +0530

    chore: install dependencies and generate build artifacts.

---
+++ backend/user/dist/model/user.model.js
@@ -0,0 +1,17 @@
+import mongoose, { Document, Schema } from "mongoose";
+const schema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+}, {
+    timestamps: true,
+});
+export const User = mongoose.model("User", schema);
+//# sourceMappingURL=user.model.js.map
